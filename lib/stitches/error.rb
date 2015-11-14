f4d8cb250c7a2ad47e909115f699b8011805c9f6
Dave Copeland <davetron5000@gmail.com>
2015-11-14 16:16:01 -0500

    move message into the exception

--- lib/stitches/error.rb
+++ lib/stitches/error.rb
@@ -1,17 +1,22 @@
 module Stitches
   class Error
-    class MissingParameter < StandardError; end
-
     attr_reader :code, :message
     def initialize(options = {})
       [:code, :message].each do |key|
         unless options.has_key?(key)
-          raise MissingParameter, "#{ self.class.name } must be initialized with :#{ key }"
+          raise MissingParameter.new(self.class,key)
         end
       end
 
       @code    = options[:code]
       @message = options[:message]
     end
+
+    class MissingParameter < StandardError
+      def initialize(klass,param_name)
+        super("#{ klass.name } must be initialized with :#{ param_name }")
+      end
+    end
+
   end
 end
