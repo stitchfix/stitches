f5892c82eb649104f1bb8538fc4b50e32b37cb1a
Cameron Jacoby <cameron.jacoby@gmail.com>
2018-10-09 09:46:51 -0500

    Add rescue_from StandardError to ApiController generator file to create Stitches error objects (#62)
    ## Problem
    
    By default, new services created by Stitches only create `Stitches` error objects for `404` errors and not any other types of errors.
    
    If creating `Stitches` errors for all types of errors isn't set up manually, consumers of the service will get a response body in an unexpected format and may not be able to parse it correctly to do whatever is needed with the error.
    
    ## Solution
    
    Add `rescue_from StandardError` to the `ApiController` generator file, and create a `Stitches` error from the exception. Also report the exception to `Bugsnag` so that it's not swallowed by rescuing it.
    
    This solution is inspired by [this discussion in the Item Price Service](https://github.com/stitchfix/item-price-service/pull/20/files#r216887615).
    
    ## Deployment Steps
    
    ### Post-Deployment
    
    - [ ] Run `rake version:minor` and `rake release`
    
    ## Original Commit Messages
    
    * Add rescue_from StandardError to ApiController generator file to create Stitches error objects
    * Remove bugsnag when rescuing StandardError since not every app using stitches will have bugsnag; add assertions for rescue_from blocks to the integration test
    * Use the .from_exception method to handle not found exceptions for consistency with how other exceptions are handled

--- lib/stitches/generator_files/app/controllers/api/api_controller.rb
+++ lib/stitches/generator_files/app/controllers/api/api_controller.rb
@@ -1,10 +1,19 @@
 class Api::ApiController < ActionController::Base
   include Stitches::Deprecation
+  #
+  # The order of the rescue_from blocks is important - ActiveRecord::RecordNotFound must come after StandardError,
+  # otherwise ActiveRecord::RecordNotFound exceptions will get rescued in the StandardError block.
+  # See the documentation for rescue_from for further explanation:
+  # https://apidock.com/rails/ActiveSupport/Rescuable/ClassMethods/rescue_from
+  # Specifically, this part: "Handlers are inherited. They are searched from right to left, from bottom to top, and up
+  # the hierarchy."
+  #
+  rescue_from StandardError do |exception|
+    render json: { errors: Stitches::Errors.from_exception(exception) }, status: :internal_server_error
+  end
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
-    respond_to do |type|
-      type.json { render json: { errors: Stitches::Errors.new([ Stitches::Error.new(code: "not_found", message: exception.message) ]) }, status: 404 }
-      type.all  { render :nothing => true, :status => 404 }
-    end
+    render json: { errors: Stitches::Errors.from_exception(exception) }, status: :not_found
   end
 
   def current_user
