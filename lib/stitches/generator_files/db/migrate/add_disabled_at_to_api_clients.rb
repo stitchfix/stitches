95f9646ab7592d291e62b8fc3e4d339b7e826032
Micah Hainline <micah.hainline@stitchfix.com>
2021-08-10 12:23:06 -0500

    Fix typo in generated code

--- lib/stitches/generator_files/db/migrate/add_disabled_at_to_api_clients.rb
+++ lib/stitches/generator_files/db/migrate/add_disabled_at_to_api_clients.rb
@@ -1,7 +1,7 @@
 <% if Rails::VERSION::MAJOR >= 5 %>
-class AddEnabledToApiClients < ActiveRecord::Migration[<%= Rails::VERSION::MAJOR %>.<%= Rails::VERSION::MINOR %>]
+class AddDisabledAtToApiClients < ActiveRecord::Migration[<%= Rails::VERSION::MAJOR %>.<%= Rails::VERSION::MINOR %>]
 <% else %>
-class AddEnabledToApiClients < ActiveRecord::Migration
+class AddDisabledAtToApiClients < ActiveRecord::Migration
 <% end %>
   def change
     add_column :api_clients, :disabled_at, "timestamp with time zone", null: true
