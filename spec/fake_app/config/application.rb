3d780763641b3f0b5d3d67ea0f0c61a098104548
David Elliott <david.elliott@stitchfix.com>
2022-06-21 09:46:43 -0700

    these configuration details don't work for newer versions of Rails (#107)
    * these configuration details don't work for newer versions of Rails
    
    * remove conflicting dep

--- spec/fake_app/config/application.rb
+++ spec/fake_app/config/application.rb
@@ -8,11 +8,9 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
-require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
