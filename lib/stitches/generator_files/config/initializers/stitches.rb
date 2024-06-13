5125d7cb554671cbe879a43a47561a1718a79ba7
Benjamin Ortiz <benjamin.ortiz@stitchfix.com>
2024-06-13 15:31:27 -0700

    more clear instructions

--- lib/stitches/generator_files/config/initializers/stitches.rb
+++ lib/stitches/generator_files/config/initializers/stitches.rb
@@ -8,8 +8,8 @@ Stitches.configure do |configuration|
   # but generally should be a string with no spaces or special characters.
   configuration.custom_http_auth_scheme = "CustomKeyAuth"
 
-  # Disable API Key feature. Useful if you're not using the auth features of
-  # the library.
+  # Disable API Key feature. Enable it to add a database backed API Key auth scheme.
+  # Be sure to run `bin/rails generate stitches:api_migration` after enabling.
   configuration.disable_api_key_support = true
 
   # Env var that gets the primary key of the authenticated ApiKey
