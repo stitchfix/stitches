b78c211b30c050229b7ee6d4ab9e7a701a93001f
Benjamin Ortiz <benjamin.ortiz@stitchfix.com>
2024-06-17 08:45:01 -0700

    only set header if api key support enabled

--- lib/stitches/spec/test_headers.rb
+++ lib/stitches/spec/test_headers.rb
@@ -6,7 +6,7 @@ class TestHeaders
       "Accept"       => full_mimetype,
       "Content-Type" => full_mimetype,
     }.tap { |headers|
-      set_authorization_header(headers,options)
+      set_authorization_header(headers,options) unless Stitches.configuration.disable_api_key_support
     }
   end
 
@@ -34,8 +34,6 @@ private
   end
 
   def set_authorization_header(headers,options)
-    return nil if Stitches.configuration.disable_api_key_support
-
     api_client_key = if options.key?(:api_client)
                        options.delete(:api_client).try(:key)
                      else
